dc34e524923ac9011469db1ddaf8854a7d367df7
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-07-26 23:28:23 +0300

    change list order

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const list = document.querySelector("ul.list");
 
 const titlesArr = [
     {id: "banks", name: "Banks"},
-    {id: "bernache", name: "Emma (Men I Trust)"},
     {id: "charli_xcx", name: "Charli XCX"},
+    {id: "bernache", name: "Emma (Men I Trust)"},
     {id: "sky_ferreira", name: "Sky Ferreira"},
     {id: "the_japanese_house", name: "The Japanese House"},
     {id: "the_weeknd", name: "The Weeknd"},
